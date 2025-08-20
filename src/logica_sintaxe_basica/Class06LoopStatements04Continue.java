a6ebc39e050d01446cb876c1e6889025b6b8b7d1
igorhgds <igorhenrique.gomes@hotmail.com>
2025-08-20 18:00:33 -0300

    Conditional Continue

---
+++ src/logica_sintaxe_basica/Class06LoopStatements04Continue.java
@@ -0,0 +1,19 @@
+package logica_sintaxe_basica;
+
+public class Class06LoopStatements04Continue {
+    //Given a car's total price, for example 20000
+    //I want to find how much and for how long I'll have to pay monthly.
+    //Condition: The monthly payment cannot be lower than 1000
+    public static void main(String[] args) {
+
+        double totalPrice = 20000;
+        for (int monthCount = (int) totalPrice; monthCount >= 1; monthCount--) {
+            double monthlyPrice = totalPrice / monthCount;
+
+            if (monthlyPrice < 1000) {
+                continue;
+            }
+            System.out.println("Months " + monthCount + " Price: " + monthlyPrice);
+        }
+    }
+}
