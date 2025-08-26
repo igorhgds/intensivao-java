d471b504c8ac8cf0cd6b516a8182e43a14b3c3c1
igorhgds <igorhenrique.gomes@hotmail.com>
2025-08-26 19:58:23 -0300

    Object Orientation - Reference Parameters pt2

--- src/classes_objetos_metodos/Bintroductionmethods/domain/StudentPrinter.java
+++ src/classes_objetos_metodos/Bintroductionmethods/domain/StudentPrinter.java
@@ -3,8 +3,11 @@ package classes_objetos_metodos.Bintroductionmethods.domain;
 
 public class StudentPrinter {
     public void print(Student student) {
+        System.out.println("-----------------");
         System.out.println("Name: " + student.name);
         System.out.println("Age: " + student.age);
         System.out.println("Gender: " + student.gender);
+
+        student.name = "Gohan";
     }
 }
--- src/classes_objetos_metodos/Bintroductionmethods/test/StudentTest01.java
+++ src/classes_objetos_metodos/Bintroductionmethods/test/StudentTest01.java
@@ -19,7 +19,11 @@ public class StudentTest01 {
         student02.age = 46;
 
         printer.print(student01);
-        System.out.println("-----------------");
+        printer.print(student02);
+
+        System.out.println("##########################");
+
+        printer.print(student01);
         printer.print(student02);
 
 //        System.out.println("Name: " + student01.name);
