ac5bc48ac57784fe10e7b28095c7ff1f3cf3d63d
igorhgds <igorhenrique.gomes@hotmail.com>
2025-09-24 21:53:18 -0300

    Exceptions (Erros, RuntimeExceptions and Exception)

---
+++ src/classes_objetos_metodos/Oexception/error/test/StackOverflowTest01.java
@@ -0,0 +1,12 @@
+package classes_objetos_metodos.Oexception.error.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void recursion(){
+        recursion();
+    }
+}
---
+++ src/classes_objetos_metodos/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,19 @@
+package classes_objetos_metodos.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        //Checked and Unchecked
+        //Checked -> all of at exceptions under "Exceptions" are checked (code not compile)
+        //Unchecked -> are exceptions that under or are it's a "RuntimeException" (caused for developers)
+
+        //NullPointerException
+        /*
+        Object obj = null;
+        System.out.println(obj.toString());*/
+
+        //ArrayIndexOutOfBoundsException
+        /*
+        int[] nums = {1,2};
+        System.out.println(nums[2]);*/
+    }
+}
