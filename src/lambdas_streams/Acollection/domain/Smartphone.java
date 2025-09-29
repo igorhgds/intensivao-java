965e54efeced83444dad709b4177fe98e39b170d
igorhgds <igorhenrique.gomes@hotmail.com>
2025-09-29 19:20:35 -0300

    Collection - Equals and HashCode

---
+++ src/lambdas_streams/Acollection/domain/Smartphone.java
@@ -0,0 +1,45 @@
+package lambdas_streams.Acollection.domain;
+
+public class Smartphone {
+    private String serialNumber;
+    private String name;
+
+    public Smartphone(String serialNumber, String name) {
+        this.serialNumber = serialNumber;
+        this.name = name;
+    }
+
+    //  if x.equals(y) == true, y.hashCode() == x.hashCode()
+    //  y.hashCode() == x.hashCode(), doesn't mean that y.equals(x) == true
+    //  x.equals(y) == false
+    //  y.hashCode() != x.hashCode(), x.equals(y) should be false
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : serialNumber.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj)  return true;
+        if (this.getClass() != obj.getClass())  return false;
+        Smartphone othersmartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(othersmartphone.serialNumber);
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/lambdas_streams/Acollection/test/EqualsTest01.java
@@ -0,0 +1,14 @@
+package lambdas_streams.Acollection.test;
+
+import lambdas_streams.Acollection.domain.Smartphone;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1BACN2", "Pixel");
+        // Smartphone s2 = s1; -> true
+        Smartphone s2 = new Smartphone("1BACN2", "Pixel"); // -> false, because reference the object(local)
+
+        System.out.println(s1.equals(s2));
+
+    }
+}
