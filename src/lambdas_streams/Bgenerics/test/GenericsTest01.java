ec4935944cee2f7890716cccab3fe211cd1f7468
Igor Henrique <ihenrique@sp.senai.br>
2025-11-10 21:24:00 -0300

    Generics - Introduction

---
+++ src/lambdas_streams/Bgenerics/test/GenericsTest01.java
@@ -0,0 +1,22 @@
+package lambdas_streams.Bgenerics.test;
+
+import lambdas_streams.Acollection.domain.Customer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+
+        List list = new ArrayList();
+        list.add("Midoriya");
+        list.add(123L);
+        list.add(new Customer("Jiraya"));
+
+        for(Object o : list) {
+            if (o instanceof Customer) {
+                Customer c = (Customer) o;
+                System.out.println(c);
+            }
+        }
+    }
+}
